3bee3b9c68a0ed2bf536e72d00e513cf4fc3988f
Lucas Neiva <lucassneiva.code@gmail.com>
2021-10-15 22:06:06 -0300

    google maps implement

--- src/App.js
+++ src/App.js
@@ -2,47 +2,58 @@ import car from './cars.png';
 import './App.css';
 import data from './frontend_data_gps.json';
 import { useEffect, useState } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-// top={positionLong} left={positionLat}
+function App(props) {
 
-function App() {
   const [positionLat, setPositionLat] = useState([]);
-
+  
   const [positionLong, setPositionLong] = useState([]);
-
+  
   useEffect(() => {
     getPos();
     console.log(positionLong, 'oi')
+    console.log(positionLat, 'ola')
   }, [positionLat, positionLong]);
-
-  // const getStyle = () => {
-    let style = {
-      width: "1em",
-      height: "1em",
-      position: "absolute",
-      top: positionLong,
-      left: positionLat,
-      backgroundColor: "red"
-    }
-  // }
   
-  const getPos = () => {
-    const infos = data.courses[0].gps;
-    infos.forEach(el => setPositionLong(...positionLong, el.longitude) );
-    infos.forEach(el => setPositionLat(...positionLat, el.latitude) );
-    console.log(positionLong, 'ooooooooooooo')
-  };
+    // let style = {
+    //   width: "1em",
+    //   height: "1em",
+    //   position: "absolute",
+    //   top: positionLong,
+    //   left: positionLat,
+    //   backgroundColor: "red"
+    // }
+    
+    const getPos = () => {
+      const infos = data.courses[0].gps;
+      infos.map(el => setPositionLong(el.longitude) );
+      infos.map(el => setPositionLat(el.latitude) );
+      console.log(positionLong, 'ooooooooooooo')
+    };
+    
+    const Car = () => <img src={car} className="car-sprite" alt="car"/>;
 
   return (
-    <div>
-      <header>
-        <div>
-        <img src={car} className="car-sprite" alt="logo" style={style}/>
-        </div>
+    <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          // bootstrapURLKeys={{ key: *** }}
+          defaultCenter={props.center}
+          defaultZoom={props.zoom}
+        >
+        <Car lat={positionLat} lng={positionLong}/>
+        </GoogleMapReact>
           {/* { infos.map(({ courses: { gps: el } }) => <p> {el.longitude} </p>)} */}
-      </header>
     </div>
   );
 }
+
+App.defaultProps = {
+  center: {
+    lat: -23.91,
+    lng: -46.27
+  },
+  zoom: 15
+};
 
 export default App;
